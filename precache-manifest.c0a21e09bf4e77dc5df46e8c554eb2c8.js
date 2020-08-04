fac870d3d33dc7237fa9b51449c6ae5e51121bca
ydjopa <ydjopa@gmail.com>
2020-08-03 20:43:55 -0400

    Updates

---
+++ precache-manifest.c0a21e09bf4e77dc5df46e8c554eb2c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "679ddcbaf206264d307cc7ae6f129c6a",
+    "url": "/Quiz-App/index.html"
+  },
+  {
+    "revision": "71ddad14e2e4e495242f",
+    "url": "/Quiz-App/static/js/2.a3e4b22d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Quiz-App/static/js/2.a3e4b22d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8cfd6f5fadf3a265cfd",
+    "url": "/Quiz-App/static/js/main.19b91d6b.chunk.js"
+  },
+  {
+    "revision": "187a2405d05605f2103a",
+    "url": "/Quiz-App/static/js/runtime-main.064bc0f6.js"
+  },
+  {
+    "revision": "a85958ea340ec34d025f981f2b722b8e",
+    "url": "/Quiz-App/static/media/bg.a85958ea.jpg"
+  },
+  {
+    "revision": "cbfcc85d30b744992c68892bacbafdf1",
+    "url": "/Quiz-App/static/media/quiz-icon.cbfcc85d.jpg"
+  }
+]);
